813ef2c2465206e6eccda695382f1f64e6674fdb
cookieboty <cookieboty@qq.com>
2025-05-12 16:04:25 +0800

    chore: 更新.gitignore文件，添加pnpm-lock.yaml、.next和.env文件的忽略规则；优化material.providers.ts文件中的异步工厂函数格式

--- apps/materialsServer/src/material/material.providers.ts
+++ apps/materialsServer/src/material/material.providers.ts
@@ -5,17 +5,20 @@ import { VirtualMaterial } from './virtual/virtual.mongo.entity';
 export const MaterialProviders = [
   {
     provide: 'PHYSICAL_MATERIAL_REPOSITORY',
-    useFactory: async (AppDataSource) => await AppDataSource.getRepository(PhysicalMaterial),
+    useFactory: async (AppDataSource) =>
+      await AppDataSource.getRepository(PhysicalMaterial),
     inject: ['MONGODB_DATA_SOURCE'],
   },
   {
     provide: 'VIRTUAL_MATERIAL_REPOSITORY',
-    useFactory: async (AppDataSource) => await AppDataSource.getRepository(VirtualMaterial),
+    useFactory: async (AppDataSource) =>
+      await AppDataSource.getRepository(VirtualMaterial),
     inject: ['MONGODB_DATA_SOURCE'],
   },
   {
     provide: 'MATERIAL_CONFIG_REPOSITORY',
-    useFactory: async (AppDataSource) => await AppDataSource.getRepository(MaterialConfig),
+    useFactory: async (AppDataSource) =>
+      await AppDataSource.getRepository(MaterialConfig),
     inject: ['MONGODB_DATA_SOURCE'],
   },
 ];
